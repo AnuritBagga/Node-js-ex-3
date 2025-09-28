cd5bc036cb5b55d1eccda8d24774311f1c8b3718
AnuritBagga <166058029+AnuritBagga@users.noreply.github.com>
2025-09-28 17:13:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import bookingRoutes from "./routes/booking.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("🎟️ Ticket Booking API Running!");
+});
+
+// Booking routes
+app.use("/api", bookingRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
